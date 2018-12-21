a5081d892a28c3c47351e3181359c112e0e9dbb6
Alena Zhmurko <alenazhmurko@gmail.com>
2018-12-21 22:44:27 +0200

    Changes for loop in task_2

--- JS1/task_2/script_2.js
+++ JS1/task_2/script_2.js
@@ -1,37 +1,47 @@
 // Спрашивать цвет/память циклически. До тех пор пока пользователь не введет валидные данные или не нажмет “Отмена“. 
-
+var inlut;
 var isCancel = false;
 var color;
 var memory;
-var price;
+var avaliable_colors = ['black','gray','gold'];
+var avaliable_ram = ['64','128','256'];
 
-var color = prompt('Input color: (black/gray/gold)');
+do {
+  var flag = false;
+  
 
-if (color == null) {
-  isCancel = true;
-} else do {
-  color = prompt('Try again! Choose another color (black/gray/gold):');
-} while (color !== 'black' || color !== 'gray' || color !== 'gold');
+  while (!avaliable_colors.includes(color)) {
+    color = prompt('Input color : (black/gray/gold)');
+    if (color == null ) {
+      isCancel = true;
+      
+      break
+    };
+   };
+  
+  if (isCancel) {
+    console.log("CANCELED after step 1");
+    color = "default";
+    memory = "256";
+    break;
+  };
+  
+  while (!avaliable_ram.includes(memory)) {
+    memory = prompt('Input memory size (64/128/256): ');
+    if (memory == null) {
+      isCansel = true;
+      break
+    };
+  };
+  if (isCancel) {
+    console.log("CANCELED after step 2");
+    color = "default";
+    memory = "256";
+    break
+  };
+  flag = true
+} while (!flag);
 
+document.write('<img src="' + color + '.jpg" width=250px><br>');
+document.write('Memory size: ' + memory + ' Gb <br>');
 
-if (color !== null){
-  memory = prompt('Input memory size (64/128/256): ');
-  if (memory == '64') {
-    price = 529;
-  } else if (memory == '128'){
-    price = 579;
-  } else if (memory == '256') {
-    price = 629;
-  } else while (memory !== '64' || memory !== '128' || memory !== '256'){
-    memory = prompt('Wrong memory size! Try again. Input memory size (64/128/256): ')
-  }
-}
-
-if (isCancel) {
-  document.write('Bad chose exeption! Try again.')
-}
-
-document.write('<h1> Image: <img width="250" src= ' + img + '> </h1>');
-document.write('<h2> Color: ' + color + '</h2>');
-document.write('<h2>Memory size: ' + memory + ' Gb</h2>');
-document.write('<h1>Price: ' + price + '$ </h1>');
